c51e690767c72eac63ef79aedcdd758172f2a3c5
dos4boss <dosscamexpert@protonmail.com>
2020-06-28 21:50:15 +0200

    add gsl and first try to write to hardware

--- src/hw_interface_helpers.cpp
+++ src/hw_interface_helpers.cpp
@@ -2,12 +2,16 @@
 #include "hw_interface.hpp"
 #include "output_helpers.hpp"
 #include "../ansi-color-kit/ansicolorkit.h"
+#include "gsl/gsl-lite.hpp"
 
 #include <algorithm>
 #include <stdexcept>
 #include <iostream>
 #include <climits>
 #include <string>
+#include <cstring>
+
+#include <sys/io.h>
 
 namespace hw_interface {
 
@@ -41,8 +45,30 @@ namespace hw_interface {
 
 
   void init(void) {
-    for(const auto & stream_info : stream_infos)
+    for(const auto & stream_info : stream_infos) {
       *(stream_info.data) = stream_info.default_data;
+      if(stream_info.interface_mode == interface_mode::IO_PORTS) {
+        auto return_value = ioperm(stream_info.address, 0x01, 1);
+        if(return_value != 0) {
+          std::cout << "ret_val " << return_value << std::strerror(errno) << std::endl;
+          if(errno == EINVAL)
+            std::cout << ansi::foreground::RED << "[I/O Ports] Address "
+                      << stream_info.address << " failed" << std::endl;
+          else if(errno == ENOMEM) {
+            std::cout << ansi::foreground::RED
+                      << "[I/O Ports] Out of Memory during access request for address "
+                      << stream_info.address << std::endl;
+            exit(1);
+          } else if (errno == EPERM) {
+            std::cout << ansi::foreground::RED
+                      << "[I/O Ports] Permission denied on access request. Make "
+              "sure to run as root. "
+                      << std::endl;
+            exit(1);
+          }
+        }
+      }
+    }
   }
 
   void read_switch(const std::string &switch_name, std::ostream &out) {
@@ -56,6 +82,7 @@ namespace hw_interface {
       out << ansi::foreground::RED << "Switch is invalid" << std::endl;
       return;
     }
+
 
     if ((stream_infos[result->stream_id].interface_mode !=
          interface_mode::IO_PORTS) &&
@@ -85,6 +112,37 @@ namespace hw_interface {
             << std::endl;
       else
         out << "Discrete Value: " << switch_trans->name << std::endl;
+    }
+  }
+
+  void write_io(const uint32_t address, const uint32_t value, const uint8_t length, enum access_width access_width, std::ostream &out) {
+    switch(access_width) {
+    case access_width::BYTE:
+      if(length != 1) {
+        out << ansi::foreground::RED << "Byte access is usually only done for 1 byte long streams." << std::endl;
+        return;
+      }
+      outb(address, gsl::narrow<uint8_t>(value));
+      out << ansi::foreground::CYAN << "[I/O Ports] Writing byte: 0x" << std::hex
+          << +gsl::narrow<uint8_t>(value) << " @ 0x" << address << std::endl;
+        break;
+    default:
+      out << ansi::foreground::RED << "[I/O Ports] Access mode currently not supported." << std::endl;
+    }
+  }
+
+  void flush_switch(enum switch_id switch_id, std::ostream &out) {
+    if(switch_id >= switch_infos.size()) {
+      out << ansi::foreground::RED << "Invalid switch id" << std::endl;
+      return;
+    }
+    const auto &stream_info = stream_infos[switch_infos[switch_id].stream_id];
+    switch(stream_info.interface_mode) {
+    case interface_mode::IO_PORTS:
+      write_io(stream_info.address, *(stream_info.data), stream_info.length, stream_info.access_width, out);
+      break;
+    default:
+      out << ansi::foreground::RED << "Interface mode not supported" << std::endl;
     }
   }
 
@@ -146,7 +204,7 @@ namespace hw_interface {
                             result->bit_length, result->bit_position,
                             switch_trans->bitstream_value);
     }
-
+    flush_switch(result->switch_id, out);
   }
 
 }; // namespace hw_interface
